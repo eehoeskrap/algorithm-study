c38623c1f96213e65e4d2704b54d7a79f168bb79
Seohee Park <eehoeskrap@gmail.com>
2022-05-27 16:39:38 +0900

    Create 1342_number_of_steps_to_reduce_a_number_to_zero.py

---
+++ leetcode/easy/1342_number_of_steps_to_reduce_a_number_to_zero.py
@@ -0,0 +1,24 @@
+class Solution1(object):
+    def numberOfSteps(self, num):
+        step = 0
+        while(num != 0):
+            step += 1
+            num = num // 2 if num % 2 == 0 else num - 1
+        print(step)
+
+class Solution2(object):
+    def numberOfSteps(self, num):
+        step = 0
+        while(num != 0):
+            step += 1
+            if num % 2 == 0:
+                num //= 2
+                print(num, "is even; divide by", 2, "and obtain", num)
+            else:
+                num -= 1
+                print(num, "is odd; divide by", 1, "and obtain", num)
+        print(step)
+if __name__ == "__main__":
+    num = 123
+    Solution1().numberOfSteps(num)
+    Solution2().numberOfSteps(num)
